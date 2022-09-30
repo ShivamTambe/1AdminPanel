2ca764237bffa45b4594c2c7c1f9fd8b2d46c7b6
unknown <shivamstambe20222@gmail.com>
2022-09-30 23:36:15 +0530

    added databas with some filter

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ const app = express();
 const bodyparser = require('body-parser');
 const mongoose = require("mongoose");
 const md5 = require("md5");
-const { userInfo } = require("os");
+// const { userInfo } = require("os");
 
 const port = process.env.PORT || 5000;
 app.use(express.static(path.join(__dirname, 'public')));
@@ -154,18 +154,16 @@ app.get("/gyms", function(req,res){
     //         return foundItems;
     //     });
     GymInfo.find().then(result =>{
-        console.log(result);
-        res.render('admin',{ item : result, type: "gyms"});
+        res.render('admin',{ item : result});
     }).catch(err => console.log(err));
 })
 app.get("/users", function(req,res){
     // GymInfo.find({}, function(err, foundItems){
-    //         // console.log(foundItems);
     //         return foundItems;
     //     });
     UserInfo.find().then(result =>{
-        console.log(result);
-        res.render('admin',{ item : result, type: "users"});
+        
+        res.render('usersdetails',{ item : result});
     }).catch(err => console.log(err));
 })
 
@@ -243,27 +241,65 @@ app.post("/login",async function(req, res){
             }
         });
 })
-console.log("HELLO");
-function allgyms(){
-    GymInfo.find({}, function(err, foundItems){
-            for(var i=0; i<foundItems.length ; i++){
-                // (foundItems[i].email +" "+ foundItems[i].password+" /n");
-                if((email == foundItems[i].email && foundItems[i].password == password) || (email == "admin@gmail.com" && password == "admin@1234")){
-                    console.log("founnd");
-                    page++;
-                    console.log(page);
-                    break;
-                }
-            } 
-            if(page >=1){
-                // res.redirect(`/index?name=${email}`)
-                res.redirect("/index");
+
+app.post("/filtersearch",function(req, res){
+    let all = req.body.all;
+    let economical = req.body.economical;
+    let premimum = req.body.premimum;
+
+    if(all=='on' || (economical=='on' && premimum=='on')){
+        GymInfo.find().then(result =>{
+            res.render('admin',{ item : result, type: "gyms"});
+        }).catch(err => console.log(err));
+    }
+    else{
+        if(economical=='on'){
+            GymInfo.find({ discountprice: {$lte:100}}).then(result =>{
+                res.render('admin',{ item : result, type: "gyms"});
+            }).catch(err => console.log(err));
+        }
+        else{
+            GymInfo.find({ discountprice: {$lte:300, $gt:100}}).then(result =>{
+                res.render('admin',{ item : result, type: "gyms"});
+            }).catch(err => console.log(err));
+        }
+    }
+})
+
+
+app.post("/filterusers",function(req, res){
+    let all = req.body.all;
+    let m = req.body.msg;
+    let t = req.body.train;
+    // console.log(all);
+    if(all=='on' || (m == 'on' && t=='on')){
+        UserInfo.find().then(result =>{
+        // console.log(result);
+            
+            res.render('usersdetails',{ item : result});
+        }).catch(err => console.log(err));
+    }
+    else{
+        // console.log(m);
+        if(m =='on'){
+            UserInfo.find({service:'Message'}).then(result =>{
+                console.log(result);
+                res.render('usersdetails',{ item : result, type: "gyms"});
+            }).catch(err => console.log(err));
+        }
+        else{
+            console.log(t);
+            if(t =='on'){
+                UserInfo.find({service:'Training'}).then(result =>{
+                    console.log(result);
+                    res.render('usersdetails',{ item : result, type: "gyms"});
+                }).catch(err => console.log(err));
             }
-            else{
-                res.redirect("/login");
-            }
-        });
-}
+        }
+    }
+})
+
+
 
 app.get("/forms",function(req,res){
     res.render("forms");
